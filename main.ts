b586a36aecd5abe0b4ff4c2629db91e11ba6fc8d
coolepicusername <102439561+coolepicusername@users.noreply.github.com>
2022-10-24 12:39:01 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,116 @@
+let sim_1 = sprites.create(img`
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+`,SpriteKind.Player)
+let sim_2 = sprites.create(img`
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4
+`, SpriteKind.Player)
+let sim_3 = sprites.create(img`
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+`, SpriteKind.Player)
+let sim_4 = sprites.create(img`
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+`, SpriteKind.Player)
+let sim_5 = sprites.create(img`
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+    a a a a a a a a a a a a a a a a
+`, SpriteKind.Player)
+
+tilemap(tilemap`level1`)
+
+sim_1.data = 1
+sim_2.data = 2
+sim_1.data = 3
+sim_4.data = 4
+sim_5.data = 5
+
+let data_x = [randint(-20, 20), randint(-20, 20), randint(-20, 20), randint(-20, 20), randint(-20, 20),];
+let data_y = [randint(-20, 20), randint(-20, 20), randint(-20, 20), randint(-20, 20), randint(-20, 20),];
+
+sim_1.vx = data_x[0]
+sim_1.vy = data_y[0]
+sim_2.vx = data_x[1]
+sim_2.vy = data_y[1]
+sim_3.vx = data_x[2]
+sim_3.vy = data_y[2]
+sim_4.vx = data_x[3]
+sim_4.vy = data_y[3]
+sim_5.vx = data_x[4]
+sim_5.vy = data_y[4]
+
+function evolve(winner : any) {
+
+}
